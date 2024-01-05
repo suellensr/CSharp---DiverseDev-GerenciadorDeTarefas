27d18ea67ffc2837d4374200856e4976233c5ebc
suellensr <suellensreis@gmail.com>
2024-01-04 22:37:28 -0300

    Add project files.

---
+++ ControleTarefas/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿namespace ControleTarefas
+{
+    public class Tarefa
+    {
+        public int IdTarefa { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        //public List<Usuario> Responsavel {  get; set; }  //Criar usuário
+        //public enum Status { get; set; }  //status sera um enum.
+
+    }
+}
+
---
+++ GerenciadorDeTarefas/Program.cs
@@ -0,0 +1,15 @@
+﻿using ControleTarefas;
+
+namespace GerenciadorDeTarefas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TarefasService tarefaService = new TarefasService();
+            Console.WriteLine(tarefaService.Caminho);
+            //List<Professor> professores = professorService.LerJsonProfessores() ?? new List<Professor>();
+        }
+    }
+}
